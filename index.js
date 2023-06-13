39ca3c53e499ade1d742d52afb5a74c8effebdcb
riaz9191 <riazahammed9191@gmail.com>
2023-06-13 18:02:37 +0600

    instructor and classes get method

--- index.js
+++ index.js
@@ -16,39 +16,46 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const instructorCollection = client.db('artoneDb').collection('instructors');
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const instructorCollection = client.db('artoneDb').collection('instructors');
+        const classesCollection = client.db('artoneDb').collection('classes');
 
+        //instructor 
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorCollection.find().toArray();
+            res.send(result);
+        })
 
-    app.get('/instructors',async(req,res)=>{
-        const result = await instructorCollection.find().toArray();
-        res.send(result);
-    })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        //classes 
+        app.get('/classes', async (req, res) => {
+            const result = await classesCollection.find().toArray();
+            res.send(result);
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-  res.send("summer camp is sitting on port")
+    res.send("summer camp is sitting on port")
 });
 app.listen(port, () => {
-  console.log(`Summer camp is running on port,${port}`)
+    console.log(`Summer camp is running on port,${port}`)
 });
