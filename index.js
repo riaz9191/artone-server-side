02711adc798fc3514d6a1e19c0ef4b65acf8eaa7
riaz9191 <riazahammed9191@gmail.com>
2023-06-13 18:41:35 +0600

    got all the data from database

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ async function run() {
         //classes 
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray();
-            res.send(result);
+            res.send(result); 
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pingedd your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
