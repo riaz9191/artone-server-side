04950c376ea42253703fe8fdcd324e86fce73137
riaz9191 <riazahammed9191@gmail.com>
2023-06-19 21:16:10 +0600

    users data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 const app = express();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 //dotenv
 require('dotenv').config();
@@ -10,6 +11,22 @@ require('dotenv').config();
 //midleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Invalid authorization' });
+    }
+    //bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'Invalid authorization' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3onslcg.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,18 +43,31 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const instructorCollection = client.db('artoneDb').collection('instructors');
         const classesCollection = client.db('artoneDb').collection('classes');
         const cartCollection = client.db('artoneDb').collection('carts');
         const userCollection = client.db('artoneDb').collection('users');
 
+        //jwt
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
 
         //user collection
-
+        app.get("/users", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user)
+            const query = { email: user.email }
+
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
                 return res.send({ message: 'User already exists' })
@@ -45,7 +75,68 @@ async function run() {
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
+        //make admin part
+        //security layers: 1. verify JWT
+        //2.email same
+        //3.check admin
+        
+        app.get('/users/admin/:email',verifyJWT,async (req, res) => {
+            const email =req.params.email;
+            if(req.decoded.email !== email) {
+                res.send({admin: false});
+            }
+            const query = { email: req.body.email}
+            const user = await userCollection.findOne(query)
+            const result = {admin: user?.role === 'admin' }
+            res.send(result)
+        })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin',
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: '',
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
 
+
+        //make instructor part
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'instructor',
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+        // app.delete('/users/instructor/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set: {
+        //             role: '',
+        //         }
+        //     }
+        //     const result = await userCollection.updateOne(filter, updatedDoc)
+        //     res.send(result)
+        // })
 
 
         //instructor 
@@ -62,16 +153,16 @@ async function run() {
 
 
         //cart collection
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([]);
             }
 
-            // const decodedEmail = req.decoded.email;
-            // if(email !== decodedEmail){
-            //   return res.status(403).send({error: true, message: 'forbidden acces'});
-            // }
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+              return res.status(403).send({error: true, message: 'Forbidden access'});
+            }
             const query = { email: email }
             const result = await cartCollection.find(query).toArray();
             res.send(result);
